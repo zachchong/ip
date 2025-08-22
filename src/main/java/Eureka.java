d8153490aa4a8102c59e5aab3aabd3d042640beb
ZachChong <chongzhuanghong@gmail.com>
2025-08-22 18:08:12 +0800

    level 0 - initial skeletal

---
+++ src/main/java/Eureka.java
@@ -0,0 +1,11 @@
+public class Eureka {
+    public static void main(String[] args) {
+        String CHATBOT_NAME = "Eureka";
+        System.out.println("_____________________________");
+        System.out.println("Hello! I'm " + CHATBOT_NAME);
+        System.out.println("What can I do for you?");
+        System.out.println("_____________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_____________________________");
+    }
+}
