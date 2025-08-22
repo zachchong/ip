0266cfb249e08ad3ec0e60d011f50853e0e9998c
ZachChong <chongzhuanghong@gmail.com>
2025-08-22 18:20:24 +0800

    level-1 echo

--- src/main/java/Eureka.java
+++ src/main/java/Eureka.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Eureka {
+
+    private static String input;
+
     public static void main(String[] args) {
         String CHATBOT_NAME = "Eureka";
         System.out.println("_____________________________");
         System.out.println("Hello! I'm " + CHATBOT_NAME);
         System.out.println("What can I do for you?");
         System.out.println("_____________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("_____________________________");
+            System.out.println(input);
+            System.out.println("_____________________________");
+            input = scanner.nextLine();
+        }
+
+        System.out.println("_____________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_____________________________");
+
+
     }
 }
