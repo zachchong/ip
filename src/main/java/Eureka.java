bc5a0082c37a01e5fccae44ff171ab88ea55ab3d
ZachChong <chongzhuanghong@gmail.com>
2025-08-23 12:46:25 +0800

    level-2 add,list

--- src/main/java/Eureka.java
+++ src/main/java/Eureka.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Eureka {
 
     private static String input;
+    private static String[] taskList = new String[100];
+    private static int taskListCount = 0;
 
     public static void main(String[] args) {
         String CHATBOT_NAME = "Eureka";
@@ -14,17 +16,30 @@ public class Eureka {
         Scanner scanner = new Scanner(System.in);
         input = scanner.nextLine();
 
-        while (!input.equals("bye")) {
-            System.out.println("_____________________________");
-            System.out.println(input);
-            System.out.println("_____________________________");
-            input = scanner.nextLine();
+        while (true) {
+            switch (input) {
+                case "bye":
+                    System.out.println("_____________________________");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("_____________________________");
+                    return;
+                case "list":
+                    System.out.println("_____________________________");
+                    for (int i = 0; i < taskListCount; i++) {
+                        System.out.println(i + 1 + ". " + taskList[i]);
+                    }
+                    System.out.println("_____________________________");
+                    input = scanner.nextLine();
+                    break;
+                default:
+                    taskList[taskListCount] = input;
+                    taskListCount++;
+                    System.out.println("_____________________________");
+                    System.out.println("added: " + input);
+                    System.out.println("_____________________________");
+                    input = scanner.nextLine();
+            }
         }
-
-        System.out.println("_____________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________");
-
 
     }
 }
