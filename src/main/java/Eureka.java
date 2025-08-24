f1d8f47646bcf5244037fe03b8e3837e288adefd
ZachChong <chongzhuanghong@gmail.com>
2025-08-24 08:58:29 +0800

    level-4,5,6

--- src/main/java/Eureka.java
+++ src/main/java/Eureka.java
@@ -1,5 +1,6 @@
 import javafx.css.Match;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,7 +8,7 @@ import java.util.regex.Pattern;
 public class Eureka {
 
     private static String input;
-    private static Task[] taskList = new Task[100];
+    private static ArrayList<Task> taskList = new ArrayList<>();
     private static int taskListCount = 0;
 
     public static void main(String[] args) {
@@ -31,80 +32,105 @@ public class Eureka {
                     System.out.println("_____________________________");
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskListCount; i++) {
-                        System.out.println(i + 1 + ". " + taskList[i].toString());
+                        System.out.println(i + 1 + ". " + taskList.get(i).toString());
                     }
                     System.out.println("_____________________________");
-
                     break;
                 default:
                     if (input.startsWith("mark")) {
                         String[] parts = input.split(" ");
                         if (parts.length > 1) {
-                            System.out.println(parts[1]);
                             int index = Integer.parseInt(parts[1]) - 1;
                             if (index >= 0 && index < taskListCount) {
-                                taskList[index].setIsDone(true);
+                                taskList.get(index).setIsDone(true);
                             }
                         }
                         System.out.println("_____________________________");
                         System.out.println("Nice! I've marked this task as done:");
-                        System.out.println(taskList[Integer.parseInt(parts[1]) - 1].toString());
+                        System.out.println(taskList.get(Integer.parseInt(parts[1]) - 1).toString());
                         System.out.println("_____________________________");
                     } else if (input.startsWith("unmark")) {
                         String[] parts = input.split(" ");
                         if (parts.length > 1) {
                             int index = Integer.parseInt(parts[1]) - 1;
                             if (index >= 0 && index < taskListCount) {
-                                taskList[index].setIsDone(false);
+                                taskList.get(index).setIsDone(false);
                             }
                         }
                         System.out.println("_____________________________");
                         System.out.println("OK, I've marked this task as not done yet:");
-                        System.out.println(taskList[Integer.parseInt(parts[1]) - 1].toString());
+                        System.out.println(taskList.get(Integer.parseInt(parts[1]) - 1).toString());
+                        System.out.println("_____________________________");
+                    } else if (input.startsWith("delete")) {
+                        String[] parts = input.split(" ");
+                        String deletedInfo = "";
+                        if (parts.length > 1) {
+                            int index = Integer.parseInt(parts[1]) - 1;
+                            if (index >= 0 && index < taskListCount) {
+                                deletedInfo = taskList.get(index).toString();
+                                taskList.remove(index);
+                                taskListCount--;
+                            }
+                        }
+                        System.out.println("_____________________________");
+                        System.out.println("Noted. I've removed this task:");
+                        System.out.println(deletedInfo);
+                        System.out.println("Now you have " + taskListCount + " tasks in the list.");
                         System.out.println("_____________________________");
                     } else {
                         if (input.startsWith("todo")) {
-                            String[] parts = input.split("\\s+", 2);
+                            Pattern p = Pattern.compile("^\\s*(?<cmd>\\w+)\\s+(?<taskName>.+)\\s*$");
+                            Matcher m = p.matcher(input);
+                            if (!m.matches()) {
+                                System.out.println("_____________________________");
+                                System.out.println("Invalid todo format. Task Name cannot be empty. Eg: todo homework");
+                                System.out.println("_____________________________");
+                                break;
+                            }
+                            String taskName = m.group("taskName");
 
-                            taskList[taskListCount] = new Todo(parts[0]);
+                            taskList.add(new Todo(taskName));
                             taskListCount++;
 
                         } else if (input.startsWith("deadline")) {
                             Pattern p = Pattern.compile("^\\s*(?<cmd>\\w+)\\s+(?<taskName>.+?)\\s+/by\\s+(?<by>.+)\\s*$");
                             Matcher m = p.matcher(input);
                             if (!m.matches()) {
-                                System.out.println("Invalid deadline format.");
-                                return;
+                                System.out.println("_____________________________");
+                                System.out.println("Invalid deadline format. Eg: deadline homework /by today");
+                                System.out.println("_____________________________");
+                                break;
                             }
                             String taskName = m.group("taskName");
                             String by = m.group("by");
 
-                            taskList[taskListCount] = new Deadline(by, taskName);
+                            taskList.add(new Deadline(by, taskName));
                             taskListCount++;
 
                         } else if (input.startsWith("event")) {
                             Pattern p = Pattern.compile("^\\s*(?<cmd>\\w+)\\s+(?<taskName>.+?)\\s+/from\\s+(?<from>.+?)\\s+/to\\s+(?<to>.+)\\s*$");
                             Matcher m = p.matcher(input);
                             if (!m.matches()) {
-                                System.out.println("Invalid event format.");
-                                return;
+                                System.out.println("_____________________________");
+                                System.out.println("Invalid event format. Eg: event orientation /from today /to tomorrow");
+                                System.out.println("_____________________________");
+                                break;
                             }
                             String taskName = m.group("taskName");
                             String from = m.group("from");
                             String to = m.group("to");
 
-                            taskList[taskListCount] = new Event(from, to, taskName);
+                            taskList.add(new Event(from, to, taskName));
                             taskListCount++;
                         }
                         System.out.println("_____________________________");
                         System.out.println("Got it. I've added this task:");
-                        System.out.println(taskList[taskListCount - 1].toString());
+                        System.out.println(taskList.get(taskListCount - 1).toString());
                         System.out.println("Now you have " + taskListCount + " tasks in the list.");
                         System.out.println("_____________________________");
                     }
 
             }
         }
-
     }
 }
