c0c7984231b5b6a0df782e8070bf95d5a8f624ef
ZachChong <chongzhuanghong@gmail.com>
2025-09-18 16:39:17 +0800

    Small tweak to achieve git standard

--- src/main/java/eureka/Eureka.java
+++ src/main/java/eureka/Eureka.java
@@ -48,6 +48,7 @@ public class Eureka {
     public void run() {
         ui.showWelcome();
         boolean isExit = false;
+        
         while (!isExit) {
             try {
                 String fullCommand = ui.readCommand();
